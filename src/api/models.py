fbf380fddbb9f27354300678329f2586def2b14f
Joni Santos <jonisantos1996@hotmail.com>
2024-10-11 17:07:56 +0000

    users

--- src/api/models.py
+++ src/api/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timezone
 
 
 db = SQLAlchemy()
@@ -8,7 +9,10 @@ class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    first_name = db.Column(db.String(), unique=False, nullable=True)
+    last_name = db.Column(db.String(), unique=False, nullable=True)
+    phone_number = db.Column(db.String(), unique=False, nullable=True)
+    country = db.Column(db.String(), unique=False, nullable=False)
 
     def __repr__(self):
         return f'<User {self.id} - {self.email}>'
@@ -16,4 +20,7 @@ class Users(db.Model):
     def serialize(self):
         return {'id': self.id,
                 'email': self.email,
-                'is_active': self.is_active}
+                'first_name': self.first_name,
+                'last_name': self.last_name,
+                'phone_number': self.phone_number,
+                'country': self.country}
